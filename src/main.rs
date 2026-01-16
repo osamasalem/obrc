b0cb051da9bffd7c545911656a07176384b57c92
osamasalem <osama__salem@hotmail.com>
2026-01-16 08:43:31 +0200

    restructuring and using 3rd party crates

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,192 @@
 use core::f32;
-use std::collections::{BTreeMap, HashMap};
-use std::io::{BufRead, BufReader, BufWriter};
-use std::string::FromUtf16Error;
+use std::collections::HashMap;
+use std::io::BufWriter;
+use std::path::Path;
+use std::thread;
 
-use rayon::iter::ParallelIterator;
-use rayon::str::ParallelString;
+use memchr::{memchr, memchr_iter, memchr2};
+use memmap2::Mmap;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::Write;
 
 #[derive(Debug)]
 struct Record {
     min: f32,
     max: f32,
-    avg: f32,
+    sum: f32,
     count: f32,
 }
 
+impl Record {
+    fn recv(&mut self, degree: f32) {
+        if self.max < degree {
+            self.max = degree
+        }
+
+        if self.min > degree {
+            self.min = degree
+        }
+
+        self.sum += degree;
+        self.count += 1.0;
+    }
+
+    fn merge(&mut self, other: &Record) {
+        if self.max < other.max {
+            self.max = other.max
+        }
+
+        if self.min > other.min {
+            self.min = other.min
+        }
+
+        self.sum += other.sum;
+        self.count += other.count;
+    }
+}
 impl Default for Record {
     fn default() -> Self {
         Self {
             min: f32::MAX,
             max: f32::MIN,
-            avg: 0.0,
+            sum: 0.0,
             count: 0.0,
         }
     }
 }
 
-// TODO: To be optimized !!
+struct MappedFile {
+    map: Mmap,
+}
+
+impl MappedFile {
+    fn new(path: impl AsRef<Path>) -> Option<Self> {
+        let file = File::open(path).ok()?;
+        let map = unsafe { memmap2::Mmap::map(&file).ok()? };
+        Some(Self { map })
+    }
+
+    fn get(&self) -> &[u8] {
+        &self.map[..]
+    }
+}
+
+#[inline]
+fn read_line(slice: &[u8]) -> (&[u8], &[u8]) {
+    //    let slice = str::from_utf8(slice).ok().unwrap();
+
+    memchr::memchr(b'\n', slice)
+        .map(|idx| {
+            let (line, rem) = slice.split_at(idx);
+            let rem = rem.get(1..).unwrap();
+            (line, rem)
+        })
+        .unwrap_or((slice, b"".as_slice()))
+}
+
+#[inline]
+fn read_values(slice: &[u8]) -> Option<(&[u8], f32)> {
+    let colon = memchr(b';', slice)?;
+    let (name, number) = slice.split_at_checked(colon)?;
+    let number = fast_float2::parse(&number[1..]).ok()?;
+    Some((name, number))
+}
+
+#[test]
+fn test_read_line() {
+    assert_eq!(read_line(b"a\nb\nc"), (b"a".as_slice(), b"b\nc".as_slice()));
+    assert_eq!(read_line(b"a\nb"), (b"a".as_slice(), b"b".as_slice()));
+    assert_eq!(read_line(b"\n"), (b"".as_slice(), b"".as_slice()));
+    assert_eq!(read_line(b""), (b"".as_slice(), b"".as_slice()));
+}
+
+#[test]
+#[should_panic]
+fn test_read_values() {
+    assert_eq!(read_values(b"A;5.0"), Some((b"A".as_slice(), 5.0)));
+    read_values(b";5.0");
+    read_values(b"A;");
+    read_values(b";");
+    read_values(b"");
+}
+
+#[inline]
+fn evenly_divide_slice(slice: &[u8], n: usize) -> Vec<&[u8]> {
+    let chunk_size = slice.len() / n;
+    let mut ret = Vec::with_capacity(n);
+    let mut remaining = slice;
+
+    while !remaining.is_empty() {
+        let addition = remaining
+            .get(chunk_size - 1)
+            .filter(|&&ch| ch != b'\n')
+            .map(|_| memchr::memchr(b'\n', &remaining[chunk_size..]).unwrap_or(remaining.len()))
+            .unwrap_or(0);
+
+        let (split, rem) = remaining
+            .split_at_checked(chunk_size + addition + 1)
+            .unwrap_or((remaining, b"".as_slice()));
+
+        ret.push(split);
+        remaining = rem;
+    }
+    ret
+}
+
 fn main() {
-    let content = fs::read_to_string("measurements-10000000.txt").unwrap();
+    let threads: usize = thread::available_parallelism().unwrap().into();
+    let file = MappedFile::new(r#"D:\measurements-1000000000.txt"#).unwrap();
+    let ret = evenly_divide_slice(file.get(), 4 * threads);
+    let output = ret
+        .par_iter()
+        .fold(
+            || HashMap::with_capacity(1000),
+            |mut hmap, &val| {
+                let mut remaining = val;
+                while !remaining.is_empty() {
+                    let line;
+                    (line, remaining) = read_line(remaining);
+                    let Some((name, degree)) = read_values(line) else {
+                        continue;
+                    };
+                    let entry: &mut Record = hmap.entry(name).or_default();
+                    entry.recv(degree);
+                }
+                hmap
+            },
+        )
+        .reduce(
+            || HashMap::with_capacity(1000),
+            |mut mother, child| {
+                for (k, v) in child {
+                    let entry = mother.entry(k).or_default();
+                    entry.merge(&v);
+                }
+                mother
+            },
+        );
 
-    let res = content
-        .par_lines()
-        .filter_map(|line| {
-            let mut split = line.split(";");
-            let first = split.next()?;
-            let second = split.next()?.parse::<f32>().ok()?;
-            Some((first, second))
-        })
-        .fold(HashMap::new, |mut acc, (station, degree)| {
-            let rec = acc.entry(station).or_insert(Record::default());
+    let mut out_file = BufWriter::new(File::create("output.csv").unwrap());
 
-            if degree > rec.max {
-                rec.max = degree
-            }
-
-            if degree < rec.min {
-                rec.min = degree
-            }
-            rec.count += 1.0;
-            rec.avg = (1.0 / rec.count) * ((rec.count - 1.0) * rec.avg + degree);
-            acc
-        })
-        .reduce(HashMap::new, |mut acc, other| {
-            other.into_iter().for_each(|(k, v)| {
-                acc.entry(k)
-                    .and_modify(|rec| {
-                        if v.min < rec.min {
-                            rec.min = v.min;
-                        }
-
-                        if v.max > rec.max {
-                            rec.max = v.max;
-                        }
-
-                        rec.avg = (rec.count * rec.avg + v.count * v.avg) / (v.count + rec.count);
-                        rec.count += v.count;
-                    })
-                    .or_insert(v);
-            });
-            acc
-        });
-
-    let mut file = BufWriter::new(File::create("output.csv").unwrap());
-    res.into_iter()
-        .for_each(|(k, Record { min, max, avg, .. })| {
-            writeln!(file, "{k};{min};{max};{avg:.1}").unwrap();
-        });
+    output.iter().for_each(
+        |(
+            name,
+            Record {
+                min,
+                max,
+                sum,
+                count,
+            },
+        )| {
+            writeln!(
+                out_file,
+                "{},{min},{max},{}",
+                str::from_utf8(name).unwrap(),
+                sum / count
+            )
+            .unwrap();
+        },
+    );
 }
